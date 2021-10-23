96bf0e2722a024a60ce08f3fca5d0c506d8e5305
BATTLEHAWK <battlehawk0_0@163.com>
2021-10-23 23:47:04 +0800

    完善题目详情页面

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,13 +5,7 @@ class indexController extends Controller {
   async get() {
     this.setTitle('首页');
     this.render('index');
-    this.setValidator(this.get, {});
   }
 }
 
-class testController extends Controller {
-  // async get() {}
-}
-
 Router.RegisterRoute('main', '/', indexController);
-Router.RegisterRoute('test', '/test', testController);
--- src/ui/lib/antd-components.js
+++ src/ui/lib/antd-components.js
@@ -1,10 +1,9 @@
-import { Button, message, Modal, notification } from 'ant-design-vue';
+import { message, Modal, notification } from 'ant-design-vue';
 import 'ant-design-vue/lib/message/style/index.less';
-import 'ant-design-vue/lib/button/style/index.less';
 import 'ant-design-vue/lib/notification/style/index.less';
 import 'ant-design-vue/lib/modal/style/index.less';
 
-const components = [Button];
+const components = [];
 
 function use(app) {
   components.forEach(com => app.use(com));
